541437fd23b134b7ce5bcd588f6387a1d8947001
Тамырлан Шуренов <tamyrlan.shurenov@sanasport.kz>
2026-06-29 17:09:40 +0500

    Add SSR config to Vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'typeof window': '"undefined"',
+  },
+  ssr: {
+    noExternal: [],
+  },
 });
